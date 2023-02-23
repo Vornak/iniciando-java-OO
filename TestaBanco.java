b0c7508f108c931370cf5009efcf7b5d4300b235
Orleandro <76212175+Vornak@users.noreply.github.com>
2023-02-23 10:37:45 -0300

    Update TestaBanco.java

--- TestaBanco.java
+++ TestaBanco.java
@@ -1,16 +1,16 @@
 public class TestaBanco {
 	public static void main(String[] args) {
-		Cliente paulo = new Cliente();
-		paulo.setNome("Paulo Silveira");
+		Cliente orleandro = new Cliente();
+		paulo.setNome("Orleandro Lincoln");
 		paulo.setCpf("222.222.222.-22");
 		paulo.setProfissao("programador");
 
-		Conta contaDoPaulo = new Conta(6646,969060);
-		contaDoPaulo.deposita(100);
+		Conta contaOrleandro = new Conta(6646,969060);
+		contaOrleandro.deposita(100);
 
-		contaDoPaulo.setTitular(paulo);
-		System.out.println(contaDoPaulo.getTitular().getNome());
-		System.out.println(contaDoPaulo.getTitular());
+		contaOrleandro.setTitular(orleandro);
+		System.out.println(contaOrleandro.getTitular().getNome());
+		System.out.println(contaOrleandro.getTitular());
 
 	}
 }
